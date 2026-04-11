aee5444ea611009430a9e3f42bcde0c5d3e9691e
Gaspar <ggutierrez@fi.uba.ar>
2026-04-11 19:47:59 -0300

    re-estructuración del código de la parte 1

--- 2026-1c-tp1/tp1_1.py
+++ 2026-1c-tp1/tp1_1.py
@@ -1,48 +1,67 @@
-def main(turnos: list[tuple[int, int]]) -> tuple[list[int], int]:
-    beneficio_total = 0
-    turnos_guardados = []
-    indices_entrada = {}
+class Turno:
+    def __init__(self,indice,beneficio,h_limite):
+        self.indice_entrada = indice
+        self.beneficio = beneficio
+        self.horario_limite = h_limite
+        
+    def ver_beneficio(self):
+        return self.beneficio
+    
+    def ver_indice_entrada(self):
+        return self.indice_entrada
 
-    # Inicializamos el diccionario de indices de entrada.
+    def ver_horario_limite(self):
+        return self.horario_limite
+
+def main(turnos: list[tuple[int, int]]) -> tuple[list[int], int]:
+    turnos_elegidos = []
+    beneficio_total = 0
+    
+    horario_maximo = 0
+    turnos_guardados = []
+
+    # Inicializamos un array con datos de tipo Turnos.
     indice = 0
-    for turno in turnos:
-        indices_entrada[indice] = turno
-        indice+=1
+    for turno_i in turnos:
+        beneficio = turno_i[0]
+        horario_limite = turno_i[1]
+        if horario_limite > horario_maximo:
+            horario_maximo = horario_limite
+        turno = Turno(indice,beneficio,horario_limite)
+        turnos_guardados.append(turno)
+        indice += 1
 
     # Ordenamos los turnos por el mayor beneficio que nos dan de mayor a menor.
-    # Obtenemos una lista ordenada con las claves ordenadas de los respectivos turnos.
-    turnos_ordenados = sorted(indices_entrada, key= lambda x: indices_entrada[x][1], reverse=True)
+    # Obtenemos una lista ordenada con los turnos ordenados según su beneficio de mayor a menor.
+    turnos_ordenados = sorted(turnos_guardados, key= lambda turno: turno.ver_beneficio(), reverse=True)
     
-    # Creamos un diccionario para ir guardando los turnos en su lugar y chequear si 
+    # Creamos una lista para ir guardando los turnos en su lugar y poder chequear si 
     # ya hay otro turno cargado en ese horario.
-    horarios_ocupados = {}
-    for i in range(len(turnos_ordenados)):
-        horarios_ocupados[i] = None
+    horarios_ocupados = [None] * len(turnos)
     
-    # Iteramos sobre los indices de los turnos ordenados.
-    # Guardamos el turno en el lugar más cercano a su tiempo limite (elección greedy) 
+    # Iteramos sobre los turnos ordenados por su beneficio de mayor a menor.
+    # Intentamos guardar el turno en el lugar más cercano a su tiempo limite (elección greedy) 
     # para llegar a la solución optima global.
     # Si intentamos guardar el turno y ya hay otro en ese mismo horario 
-    # intentamos buscar si hay lugar en horarios anteriores utilizando el diccionario
-    # "horarios_ocupados".
-    for indice in turnos_ordenados:
-        horario_limite = indices_entrada[indice][0]
-        max_horario_posible = horario_limite-1
+    # buscamos si hay lugar en horarios anteriores utilizando
+    # la lista "horarios_ocupados".
+    for turno in turnos_ordenados:
+        horario_limite = turno.horario_limite()
+        horario_ideal = horario_limite-1
 
-        while max_horario_posible >= 0 and horarios_ocupados[max_horario_posible] != None:
-            max_horario_posible -= 1
+        while horario_ideal >= 0 and horarios_ocupados[horario_ideal] != None:
+            horario_ideal -= 1
 
-        if max_horario_posible < 0:
+        if horario_ideal < 0:
             continue
 
-        horarios_ocupados[max_horario_posible] = "horario ocupado"
+        horarios_ocupados[horario_ideal] = "horario ocupado"
         
-        beneficio_total += indices_entrada[indice][1]
-        turnos_guardados.append(indice)
+        beneficio_total += turno.ver_beneficio()
+        turnos_elegidos.append(turno.ver_indice_entrada())
 
-    return turnos_guardados,beneficio_total
+    return turnos_elegidos,beneficio_total
 
 if __name__ == "__main__":
     print(main([(1,20),(1,10),(5,30),(5,30),(5,30),(5,30)]))
     print(main([(1, 10), (2, 10)]))
-    
